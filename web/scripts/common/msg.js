98fd60905804214ccea09822dbac567b2ddce448
sofosogo <sofosogo@gmail.com>
2012-02-13 21:22:14 +0800

    *

--- web/scripts/common/msg.js
+++ web/scripts/common/msg.js
@@ -1,24 +1,26 @@
 define(function(require, exports, module){
-    var $msg = $("div.alert"),
-        temp = dit.create( $msg[0] ),
-        type,
-        tick;
+    var $alerts = $("div.alerts");
+    var $alert = $("div.alert").removeClass("hide").remove();
     ["warning", "error", "success", "info"].forEach(function(it){
-       exports[it] = function( msg, second ){
-           temp.fill( {type: it, msg: msg} );
-           $msg.show();
-           clearTimeout( tick );
+        exports[it] = function( msg, second ){
+            var $a = $alert.clone()
+                .appendTo( $alerts );
+            $a.addClass( "alert-" + it )
+                .find("p").html( msg );
            if( second !== 0 ){
-               tick = setTimeout(function(){
-                   $msg.hide();
+               setTimeout(function(){
+                   $a.hide();
                }, (second||3) * 1000);
            }
-           
        }
     });
-    
-    $msg.find("a").click(function(){
-        $msg.hide();
+    $alerts.delegate("a", "click", function(e){
+        $(this).parent().remove();
     });
+    
+    //exports.info("info", 0);
+    //exports.success("success", 3);
+    //exports.error("error", 4);
+    //exports.warning("warning", 0);
 });
 
